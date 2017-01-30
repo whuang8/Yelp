417eb6fa67da623063f80d62a7dc0efeac8c1971
whuang8 <huang554@purdue.edu>
2017-01-30 01:37:10 -0500

    Add details page for businesses

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -92,14 +92,14 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         searchBar.becomeFirstResponder()
         self.navigationItem.titleView = searchBar
     }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detailVC = segue.destination as? DetailViewController
+        let cell = sender as? BusinessCell
+        let indexPath = tableView.indexPath(for: cell!)
+        let business = self.businesses[(indexPath?.row)!]
+    }
 }
